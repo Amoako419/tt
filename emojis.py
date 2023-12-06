19d27bda464735c39c02f901601e2b6f79111443
Amoako Heskey <113794430+Amoako419@users.noreply.github.com>
2023-12-06 01:48:53 +0000

    Creating emojis using python
    But first install emojis
    using pip
    pip install emoji

---
+++ emojis.py
@@ -0,0 +1,7 @@
+import emoji
+print(emoji.emojize('Am a :thumbs_up:'))
+print(emoji.emojize('you are :thumbs_down:'))
+print(emoji.emojize(':grinning_face: :grinning_face_with_big_eyes: :grinning_face_with_smiling_eyes: :rolling_on_the_floor_laughing:'))
+print(emoji.emojize('Hello angel :smiling_face_with_halo:'))
+print(emoji.emojize('I am a :smiling_face_with_sunglasses: '))
+print(emoji.emojize('The weather is awesome :cold_face:'))
