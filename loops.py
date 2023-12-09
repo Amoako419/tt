95f3f31b80c4b67a3942237fc65feaec64b4193f
Amoako Heskey <113794430+Amoako419@users.noreply.github.com>
2023-12-09 01:44:30 +0000

    Loops in python

---
+++ loops.py
@@ -0,0 +1,20 @@
+# i = 3
+# while i != 0:
+#     print("meow")
+#     i=i-1
+
+# for i in range(30):
+#     for j in range(20):
+#         print('#', end="")
+#     print()    
+
+# print("meow\n"* 4,end="")
+
+
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")       
